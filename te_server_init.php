6c58312bbe362b38e94c5ecc7777aea7ff29d991
Jérôme Augé <jerome.auge@anakeen.com>
2009-07-08 17:03:57 +0200

    Changed rc init script, and te_server_init.php, to use pg_service name

--- te_server_init.php
+++ te_server_init.php
@@ -16,27 +16,15 @@ $targ=getArgv($argv);
 $dbaccess=$targ["db"];
 $dbid=@pg_connect($dbaccess);
 if ($dbid) {
-  exit(1); // already create
- } else {
-
-
-  $dbcreate=php2DbCreateSql($dbaccess);
-  $cmd=sprintf("createdb %s",$dbcreate);
-  system($cmd,$retval);	
-  if ($retval!=0) {
-    exit($retval);
-  }
-  $dbid=@pg_connect($dbaccess);
-  if ($dbid) {
-    $o=new Task($dbaccess);
-    pg_query($dbid,$o->sqlcreate);
-    $o=new Engine($dbaccess);
-    pg_query($dbid,$o->sqlcreate);
-    $sqlinit=file_get_contents("TE/engine_init.sql",true);
-    pg_query($dbid,$sqlinit);
-    
-  }
+  $o=new Task($dbaccess);
+  pg_query($dbid,$o->sqlcreate);
+  $o=new Engine($dbaccess);
+  pg_query($dbid,$o->sqlcreate);
+  $sqlinit=file_get_contents("TE/engine_init.sql",true);
+  pg_query($dbid,$sqlinit);
   exit(0);
- }
+}
+echo sprintf("Error: could not connect to database '%s'!", $dbaccess);
+exit(1);
 
 ?>
