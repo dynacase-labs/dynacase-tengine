9d7df8c8c9545ef823d7915c756fba56a247789e
Matthieu Codron <matthieu.codron+dev@anakeen.com>
2013-02-05 17:45:23 +0100

    beautification (closes #3456)

--- lib/TE/Class.Histo.php
+++ lib/TE/Class.Histo.php
@@ -1,47 +1,48 @@
 <?php
-/**
- * Tranformation Engine Historic
- *
- * @author Anakeen 2005
- * @version $Id: Class.Histo.php,v 1.2 2007/06/06 18:12:01 eric Exp $
+/*
+ * @author Anakeen
  * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License
- * @package TE
- */
-/**
+ * @package FDL
  */
 
+require_once "Class.PgObj.php";
 
-include_once("Class.PgObj.php");
-Class Histo extends PgObj {
-  public $fields = array ( "tid", 
-			   "comment"// comment text
-			   );
-  public $sup_fields = array ("date");
-  /**
-   * task identificator
-   * @public string
-   */
-  public $tid;  
-  		  
-		  
-  /**
-   * description of the action
-   * @public string
-   */
-  public $comment;
-  
-
-
-  public $id_fields = array ("tid","date");
-
-  public $dbtable = "histo";
-
-
-  public $sqlcreate = "
-create table histo ( tid int not null,   
-                   date timestamp default now(),
-                   comment text  );";
-
-
+Class Histo extends PgObj
+{
+    public $fields = array(
+        "tid",
+        "comment"
+        // comment text
+        
+    );
+    public $sup_fields = array(
+        "date"
+    );
+    /**
+     * task identificator
+     * @public string
+     */
+    public $tid;
+    /**
+     * description of the action
+     * @public string
+     */
+    public $comment;
+    
+    public $id_fields = array(
+        "tid",
+        "date"
+    );
+    
+    public $dbtable = "histo";
+    
+    public $sqlcreate = <<< 'SQL'
+CREATE TABLE histo (
+    tid INT NOT NULL,
+    date TIMESTAMP DEFAULT now(),
+    comment TEXT
+);
+SQL;
+    
 }
 ?>
